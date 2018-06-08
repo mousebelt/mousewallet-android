47358baba7d1325ab28e309c4848556919f7e9c8
mdev0730 <hashming0730@yahoo.com>
2018-06-08 18:24:31 +0300

    integrate mnemonic from sdk

--- app/src/main/java/com/norestlabs/restlesswallet/ui/Mnemonic2Activity.java
+++ app/src/main/java/com/norestlabs/restlesswallet/ui/Mnemonic2Activity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.norestlabs.restlesswallet.R;
 import com.norestlabs.restlesswallet.utils.Utils;
@@ -28,14 +29,15 @@ public class Mnemonic2Activity extends AppCompatActivity {
     @ViewById
     TagContainerLayout tagContainerLayout1, tagContainerLayout2;
 
+    String[] tags;
+
     @AfterViews
     protected void init() {
         setSupportActionBar(toolbar);
         txtTitle.setText(R.string.mnemonic);
 
-        final String[] tags = getResources().getStringArray(R.array.tags);
-        Utils.randomArray(tags);
-        tagContainerLayout2.setTags(tags);
+        tags = getIntent().getStringArrayExtra("tags");
+        tagContainerLayout2.setTags(Utils.randomArray(tags));
 
         tagContainerLayout1.setOnTagClickListener(new TagView.OnTagClickListener() {
             @Override
@@ -73,12 +75,30 @@ public class Mnemonic2Activity extends AppCompatActivity {
         });
     }
 
+    private void verifyTags() {
+        if (tagContainerLayout1.getTags().size() != tags.length) {
+            showFailedError();
+            return;
+        }
+        for (int i = 0; i < tags.length; i ++) {
+            if (!tagContainerLayout1.getTags().get(i).equals(tags[i])) {
+                showFailedError();
+                return;
+            }
+        }
+        Intent intent = new Intent(this, VerificationActivity_.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void showFailedError() {
+        Toast.makeText(this, "Your mnemonic not match!", Toast.LENGTH_SHORT).show();
+    }
+
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btnContinue:
-                Intent intent = new Intent(this, VerificationActivity_.class);
-                startActivity(intent);
-                finish();
+                verifyTags();
                 break;
             case R.id.btnBack:
                 onBackPressed();
--- app/src/main/java/com/norestlabs/restlesswallet/utils/Utils.java
+++ app/src/main/java/com/norestlabs/restlesswallet/utils/Utils.java
@@ -4,13 +4,15 @@ import java.util.Random;
 
 public class Utils {
 
-    public static void randomArray(String[] arr) {
+    public static String[] randomArray(String[] arr) {
+        final String[] randomArr = arr.clone();
         Random rnd = new Random();
-        for (int i = arr.length - 1; i > 0; i --) {
+        for (int i = randomArr.length - 1; i > 0; i --) {
             final int index = rnd.nextInt(i + 1);
-            String a = arr[index];
-            arr[index] = arr[i];
-            arr[i] = a;
+            String a = randomArr[index];
+            randomArr[index] = randomArr[i];
+            randomArr[i] = a;
         }
+        return randomArr;
     }
 }
