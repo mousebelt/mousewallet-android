9f7b18a93f270fac50fe79bb42f2e993f6c60185
mdev0730 <hashming0730@yahoo.com>
2018-06-18 23:42:13 +0300

    add sdk dependency in gradle

---
+++ app/src/main/java/com/norestlabs/restlesswallet/ui/SplashActivity.java
@@ -0,0 +1,12 @@
+package com.norestlabs.restlesswallet.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
