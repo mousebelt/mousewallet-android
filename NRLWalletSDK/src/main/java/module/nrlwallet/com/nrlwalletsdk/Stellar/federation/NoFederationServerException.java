387478a732d5a90a5d74ccd6f825831569321884
mdev0730 <hashming0730@yahoo.com>
2018-06-19 13:42:23 +0300

    remove sdk folder from project

--- NRLWalletSDK/src/main/java/module/nrlwallet/com/nrlwalletsdk/Stellar/federation/NoFederationServerException.java
+++
@@ -1,7 +0,0 @@
-package module.nrlwallet.com.nrlwalletsdk.Stellar.federation;
-
-/**
- * Federation server was not found in stellar.toml file.
- */
-public class NoFederationServerException extends RuntimeException {
-}
